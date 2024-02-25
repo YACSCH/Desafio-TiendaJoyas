d669d5f2abda92f258ebd60184c1ef8c037d9688
Yamil Castillo <yamilcs@gmail.com>
2024-02-24 22:16:50 -0300

    subiendo Desafio-TiendaJoyas cidificando middleware

---
+++ middlewares/report.js
@@ -0,0 +1,14 @@
+const getActivity = async (req, res, next) => {
+
+    const parametros = req.params
+    const query = req.query
+    const url = req.url
+    console.log(
+      `Hoy ${new Date()} 
+      se ha recibido una consulta de la ruta ${url}
+      con los parámetros:`, parametros , ` y con datos de query`, query  
+    )
+    next()
+  }
+  
+  export { getActivity }
--- routes/jewelRoutes.js
+++ routes/jewelRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { getJewels, getJewelsFilter }  from '../src/controllers/jewelsController.js'
+import { getActivity } from '../middlewares/report.js';
 
 const router = express.Router();
 
-router.get('/joyas', getJewels )
-router.get('/joyas/filtros', getJewelsFilter)
+router.get('/joyas', getActivity, getJewels )
+router.get('/joyas/filtros', getActivity,  getJewelsFilter)
 
 
 export default router;
